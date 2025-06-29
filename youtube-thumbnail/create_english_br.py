37b464a02ab8350f843a2c9a14bbd84da427349d
Mike Z. Luo <digi2nomad@yahoo.com>
2025-06-28 23:25:00 -0400

    change args

--- youtube-thumbnail/create_english_br.py
+++ youtube-thumbnail/create_english_br.py
@@ -3,42 +3,29 @@ import sys
 
 from PIL import Image, ImageDraw, ImageFont
 
-TEMPLATE = "image/Website_Jin_book_talk.jpeg"
-FONTFILE = "font/Rockstar-ExtraBold.otf"
+TEMPLATE = "image/Wang_gungwu.png"
 OUTPUT = "image/thumbnail-english.png"
-START_OFFSET_TEXT = (850, 125)
-TEXTCOLOR = (255, 0, 0, 255)
-LINE_SPACING = 140
-SEPARATOR = "|"
 
-def create_thumbnail(text,
-                     font_size,
+def create_thumbnail(
                      template=TEMPLATE,
                      output=OUTPUT,
-                     fontfile=FONTFILE,
-                     text_color=TEXTCOLOR,
-                     start_offset=START_OFFSET_TEXT,
-                     line_spacing=LINE_SPACING):
+                     ):
     base = Image.open(template).convert('RGBA')
     image = Image.new('RGBA', base.size)
-    font = ImageFont.truetype(fontfile, font_size)
     draw = ImageDraw.Draw(image)
-    offset = start_offset
 
-    #for i, line in enumerate(text.split(SEPARATOR)):
-    #    left, top = offset
-    #    top += i * line_spacing
-    #    new_offset = (left, top)
-    draw.text((100,1050), "Book Review:",  
-              font=ImageFont.truetype("font/Gendy.otf", 175), fill="yellow") 
-    draw.text((100,1250), "A princeling explains",  
-              font=ImageFont.truetype("font/Gendy.otf", 155), fill="yellow") 
-    draw.text((100,1420), "to the West",  
-              font=ImageFont.truetype("font/Gendy.otf", 155), fill="yellow") 
-    draw.text((100,1590), "How China succeeded",  
-              font=ImageFont.truetype("font/Gendy.otf", 155), fill="yellow") 
-    draw.text((30, 1800), "Sampan Review",  
-              font=ImageFont.truetype("font/Bohme-Rounded.ttf", 165), fill="white") 
+    draw.text((750,10), "Book Review:",  
+              font=ImageFont.truetype("font/Gendy.otf", 65), fill="yellow") 
+    draw.text((1150,25), "Ep0",  
+              font=ImageFont.truetype("font/Gendy.otf", 45), fill="white") 
+    draw.text((850,90), "Prof. Wang Gungwu &",  
+              font=ImageFont.truetype("font/Gendy.otf", 65), fill="yellow") 
+    draw.text((950,170), "The experience of",  
+              font=ImageFont.truetype("font/Gendy.otf", 65), fill="yellow") 
+    draw.text((1050,250), "Chinese diaspora",  
+              font=ImageFont.truetype("font/Gendy.otf", 65), fill="yellow") 
+    draw.text((30, 800), "Sampan Review",  
+              font=ImageFont.truetype("font/Bohme-Rounded.ttf", 75), fill="white") 
 
     out = Image.alpha_composite(base, image)
 
@@ -46,7 +33,4 @@ def create_thumbnail(text,
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} 'title text seperated with bars' 'text font size(e.g.: 165)'")
-        sys.exit(1)
-    create_thumbnail(sys.argv[1], int(sys.argv[2]))
+    create_thumbnail()
